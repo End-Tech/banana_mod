85cfd9f715f57b624f9fc5470f0f8c048c151b3d
makeitworkAndris <andrisbarath5@gmail.com>
2021-05-24 20:33:41 +0200

    added banana_log

--- src/main/java/net/fabricmc/example/BananaMod.java
+++ src/main/java/net/fabricmc/example/BananaMod.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.PillarBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -16,11 +17,10 @@ public class BananaMod implements ModInitializer {
 
 	public static final String MOD_NAME = "banana-mod";
 
-	public static final Item FABRIC_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC));
 	public static final Item BANANA_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC));
 
 	public static final Block BANANA_LEAVES = new Block(FabricBlockSettings.of(Material.LEAVES).strength(2.0f));
-	public static final Block BANANA_LOG = new Block(FabricBlockSettings.of(Material.WOOD).strength(2.0f));
+	public static final Block BANANA_LOG = new PillarBlock(FabricBlockSettings.of(Material.WOOD).strength(2.0f));
 
 	@Override
 	public void onInitialize() {
@@ -40,8 +40,8 @@ public class BananaMod implements ModInitializer {
 	}
 
 	public void registerItems(){
-		Registry.register(Registry.ITEM, new Identifier(MOD_NAME, "fabric_item"), FABRIC_ITEM);
 		Registry.register(Registry.ITEM, new Identifier(MOD_NAME, "banana_item"), BANANA_ITEM);
 		Registry.register(Registry.ITEM, new Identifier(MOD_NAME, "banana_leaves"), new BlockItem(BANANA_LEAVES, new FabricItemSettings().group(ItemGroup.MISC)));
+		Registry.register(Registry.ITEM, new Identifier(MOD_NAME, "banana_log"), new BlockItem(BANANA_LOG, new FabricItemSettings().group(ItemGroup.MISC)));
 	}
 }
